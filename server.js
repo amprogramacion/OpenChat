c490d2a55349692aec3583556465a0a9e9637ec3
Angel <Angel@DESKTOP-TK7E42T>
2024-06-29 15:44:37 +0200

    Chatgpt

--- server.js
+++ server.js
@@ -1,59 +1,55 @@
 // server.js
 const express = require('express');
-const http = require('http');
 const https = require('https');
 const socketIo = require('socket.io');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
-
-// Configurar CORS
 app.use(cors());
-
-// Servir archivos estáticos
 app.use(express.static('public'));
 
-// Crear el servidor HTTP o HTTPS
 const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert.pem'))
 };
+
 const server = https.createServer(options, app);
 const io = socketIo(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
 });
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3000;
 
 let users = {};
 
 io.on('connection', (socket) => {
-    console.log('Nuevo cliente conectado');
+  console.log('Nuevo cliente conectado');
 
-    socket.on('new user', (username) => {
-        users[socket.id] = username;
-        io.emit('update users', users);
-    });
+  socket.on('new user', (username) => {
+    users[socket.id] = username;
+    io.emit('update users', users);
+  });
 
-    socket.on('chat message', (data) => {
-        const message = {
-            user: data.user,
-            msg: data.msg
-        };
-        io.emit('chat message', message);
-    });
+  socket.on('chat message', (data) => {
+    const message = {
+      user: data.user,
+      msg: data.msg
+    };
+    io.emit('chat message', message);
+  });
 
-    socket.on('disconnect', () => {
-        delete users[socket.id];
-        io.emit('update users', users);
-        console.log('Cliente desconectado');
-    });
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    io.emit('update users', users);
+    console.log('Cliente desconectado');
+  });
 });
 
 server.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
